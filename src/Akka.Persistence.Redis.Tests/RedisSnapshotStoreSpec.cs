91459035b8b456f13163fd0b4219b5673c5dee3a
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2016-03-12 18:33:47 +0100

    init commit

---
+++ src/Akka.Persistence.Redis.Tests/RedisSnapshotStoreSpec.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Akka.Configuration;
+using Akka.Persistence.Redis.Snapshot;
+using Akka.Persistence.TestKit.Snapshot;
+using Akka.Util.Internal;
+using RedisBoost;
+using Xunit.Abstractions;
+
+namespace Akka.Persistence.Redis.Tests
+{
+    public class RedisSnapshotStoreSpec : SnapshotStoreSpec
+    {
+        private static readonly AtomicCounter Counter = new AtomicCounter(0);
+        private static readonly string ConfigFormat = @"akka.persistence.snapshot-store {{
+            plugin = ""akka.persistence.snapshot-store.redis""
+            redis {{ 
+		        class = ""Akka.Persistence.Redis.Snapshot.RedisSnapshotStore, Akka.Persistence.Redis"",
+		        dispatcher = ""akka.actor.default-dispatcher""
+		        connection-string=""data source = localhost:6379;initial catalog={0}""
+            }}
+        }}";
+
+        public RedisSnapshotStoreSpec(ITestOutputHelper output)
+            : base(ConfigurationFactory.ParseString(string.Format(ConfigFormat, Counter.IncrementAndGet())), "RedisSnapshotStoreSpec", output: output)
+        {
+            TestSetup.Cleanup(RedisPersistence.Get(Sys).SnapshotStoreSettings);
+            Initialize();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            TestSetup.Cleanup(RedisPersistence.Get(Sys).SnapshotStoreSettings);
+        }
+    }
+}
---
+++ src/Akka.Persistence.Redis/Snapshot/RedisSnapshotStore.cs
@@ -0,0 +1,71 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Akka.Persistence.Snapshot;
+using RedisBoost;
+using RedisBoost.Core.Serialization;
+
+namespace Akka.Persistence.Redis.Snapshot
+{
+    public class RedisSnapshotStore : SnapshotStore
+    {
+        private readonly RedisSettings _settings = RedisPersistence.Get(Context.System).SnapshotStoreSettings;
+        private readonly IRedisClientsPool _pool;
+        private readonly BasicRedisSerializer _serializer;
+
+        public RedisSnapshotStore()
+        {
+            _serializer = new RedisPersistenceSerializer(Context.System);
+            _pool = RedisClient.CreateClientsPool();
+        }
+
+        protected override void PostStop()
+        {
+            base.PostStop();
+            _pool.Dispose();
+        }
+
+        protected override async Task<SelectedSnapshot> LoadAsync(string persistenceId, SnapshotSelectionCriteria criteria)
+        {
+            var client = await CreateRedisClientAsync();
+            var snapshots = await client.ZRevRangeAsync(SnapshotKey(persistenceId), 0L, criteria.MaxSequenceNr);
+            var found = snapshots
+                .Select(bulk => bulk.As<SelectedSnapshot>())
+                .FirstOrDefault(snapshot => snapshot.Metadata.Timestamp <= criteria.MaxTimeStamp && snapshot.Metadata.SequenceNr <= criteria.MaxSequenceNr);
+
+            return found;
+        }
+
+        protected override async Task SaveAsync(SnapshotMetadata metadata, object snapshot)
+        {
+            var client = await CreateRedisClientAsync();
+            var selectedSnapshot = new SelectedSnapshot(metadata, snapshot);
+            await client.ZAddAsync(SnapshotKey(metadata.PersistenceId), metadata.SequenceNr, selectedSnapshot);
+        }
+
+        protected override async Task DeleteAsync(SnapshotMetadata metadata)
+        {
+            var client = await CreateRedisClientAsync();
+            await client.ZRemRangeByScoreAsync(SnapshotKey(metadata.PersistenceId), metadata.SequenceNr, metadata.SequenceNr);
+        }
+
+        protected override async Task DeleteAsync(string persistenceId, SnapshotSelectionCriteria criteria)
+        {
+            var client = await CreateRedisClientAsync();
+            var snapshotKey = SnapshotKey(persistenceId);
+            var snapshots = await client.ZRevRangeAsync(snapshotKey, 0L, criteria.MaxSequenceNr);
+            var found = snapshots
+                .Where(bulk => bulk.ResponseType == ResponseType.Bulk)
+                .Select(bulk => bulk.As<SelectedSnapshot>())
+                .Where(snapshot => snapshot.Metadata.Timestamp <= criteria.MaxTimeStamp && snapshot.Metadata.SequenceNr <= criteria.MaxSequenceNr)
+                .ToArray();
+            await client.ZRemAsync(snapshotKey, found);
+        }
+
+        private string SnapshotKey(string persistenceId) => _settings.KeyPrefix + ":" + persistenceId;
+
+        private Task<IRedisClient> CreateRedisClientAsync()
+        {
+            return _pool.CreateClientAsync(_settings.ConnectionString, _serializer);
+        }
+    }
+}
